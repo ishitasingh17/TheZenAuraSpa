7bca87d882dcb10c517c5bc2d93382e41a6711b4
Ishita <ishitasingh5688@gmail.com>
2026-08-13 15:10:40 +0400

    Fix ServiceSelection props

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,6 @@ function App() {
         setScreen={setScreen}
         selectedReason={selectedReason}
         setSelectedReason={setSelectedReason}
-        setSelectedBeautyCategory={setSelectedBeautyCategory}
       />
     );
   }
